c6b59f8eea97d6499a0fd8531734a9d7e21cffbe
ttdoni99 <43473991+ttdoni99@users.noreply.github.com>
2018-09-23 13:10:47 +0200

    Create Valtozok csereje szorzattal.cpp

---
+++ Valtozok csereje szorzattal.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	
+
+	int a = 8;
+	int b = 1;
+	cout<<"Elotte : "<< a << " "<< b << endl;
+	a = a * b;
+	b = a / b;
+	a = a / b;
+
+	cout <<"Utana : "<< a << " " << b << endl;
+} 
