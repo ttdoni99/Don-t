3af7ce722b61157d08c463be2b531ef8ef089c2c
ttdoni99 <43473991+ttdoni99@users.noreply.github.com>
2018-09-23 13:24:16 +0200

    Create valtozok_csereje _külömbségel.cpp

---
+++ valtozok_csereje _külömbségel.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int a,b,c;
+cout <<"Kérlek írj két számot:\n";
+cin>> a;
+cin>> b;
+c=a;
+a=(a+b)-a;
+b=(b+c)-b; //2+1-2
+cout <<a<<"\n";
+cout<<b;
+}
